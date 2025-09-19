12f2d72bf4327a4315495f154c360c8bb07b02dd
JustynaGargula <s221391@student.uek.krakow.pl>
2025-09-19 14:13:01 +0200

    Improved exercise 5: error catching, loading message

--- src/components/ApiUsers.jsx
+++ src/components/ApiUsers.jsx
@@ -1,15 +1,29 @@
 import React, {useEffect} from "react";
 
-
 export default function ApiUsers(){
-  const [users, setUsers] = React.useState([]);
   const apiUrl = "https://fakerapi.it/api/v2/users";
   const apiName= "Faker API";
+
+  const [users, setUsers] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
   const getUsers = () => {
+    setLoading(true);
+    setError(null);
     fetch(apiUrl)
-      .then(res => res.json())
-      .then(rjson => {setUsers(rjson.data)});
+      .then(res => {
+        if(!res.ok){
+          throw new Error("Could not load users.");
+        } else {
+          return res.json()
+        }
+        })
+      .then(rjson => {setUsers(rjson.data)})
+      .catch((err) => setError(err.message))
+      .finally(() => {setLoading(false)});
   }
+
   useEffect(() => {
     getUsers();
   }, []);
@@ -17,16 +31,21 @@ export default function ApiUsers(){
   return (
     <>
       <h1>Users from API</h1>
-      <ul>
-        {users.map((user) => { return (
+      { loading && <p>Loading users...</p> }
+      { error ? <p>Error: {error}</p> : (
+        <ul>
+          {users.map((user) => (
           <li className="field" key={user.id}>
-            <p className="bold">{user.username}</p><ul>
-            <li>Name: {user.firstname} {user.lastname}</li>
-            <li>Email: {user.email}</li>
-          </ul></li>
-          )})}
-      </ul>
-      <button onClick={getUsers}>Refresh users</button>
+            <p className="bold">{user.username}</p>
+            <div className="indented-field">
+              <p>Name: {user.firstname} {user.lastname}</p>
+              <p>Email: {user.email}</p>
+            </div>
+          </li>
+          ))}
+        </ul>) }
+
+      <button onClick={getUsers} disabled={loading} >Refresh users</button>
 
       <footer>Users were fetched from <a href={apiUrl}>{apiName}</a></footer>
     </>
